154962cc85a1f3d9256cd8a13bf879c14de5cc4e
Brennan Douglas <brennan.douglas@oregonstate.edu>
2017-05-30 15:17:45 -0700

    Completed tutorial

--- TypeScriptTraining/TypeScriptTraining/app.js
+++ TypeScriptTraining/TypeScriptTraining/app.js
@@ -3,19 +3,68 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+window.onload = function () {
+    var el = document.getElementById('content');
+    var jsx = React.createElement(ToDoList, { name: "Brennan" });
+    ReactDOM.render(jsx, el);
+};
 var ToDoList = (function (_super) {
     __extends(ToDoList, _super);
-    function ToDoList() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function ToDoList(props) {
+        var _this = _super.call(this, props) || this;
+        _this.onInputChange = function (e) {
+            var newState = __assign({}, _this.state, { newItemName: e.currentTarget.value });
+            _this.setState(newState);
+        };
+        _this.addItem = function () {
+            var newItems = _this.state.items.concat([{ name: _this.state.newItemName, completed: false }]);
+            var newState = __assign({}, _this.state, { items: newItems, newItemName: "" });
+            _this.setState(newState);
+        };
+        _this.toggleItemCompleted = function (index) {
+            var newItems = _this.state.items.slice();
+            newItems[index] = __assign({}, newItems[index], { completed: !newItems[index].completed });
+            var newState = __assign({}, _this.state, { items: newItems });
+            _this.setState(newState);
+        };
+        _this.state = {
+            items: [
+                { name: "Milk", completed: false },
+                { name: "Eggs", completed: false },
+                { name: "Bread", completed: false },
+            ],
+            newItemName: ""
+        };
+        return _this;
     }
     ToDoList.prototype.render = function () {
-        return (React.createElement("div", null, "It seems there is still much to do."));
+        var _this = this;
+        var renderItem = function (item, index) {
+            var itemClass = item.completed ? "completed-item" : "incompleted-item";
+            itemClass += " todo-item";
+            var checkMark = item.completed ? " ✓" : "";
+            return (React.createElement("li", null,
+                React.createElement("div", { className: itemClass, onClick: function () { return _this.toggleItemCompleted(index); } },
+                    item.name,
+                    checkMark)));
+        };
+        var itemsJSX = this.state.items.map(function (item, i) { return renderItem(item, i); });
+        return (React.createElement("div", null,
+            React.createElement("h3", null,
+                this.props.name,
+                "'s To-Do List"),
+            React.createElement("ul", null, itemsJSX),
+            React.createElement("input", { value: this.state.newItemName, onChange: this.onInputChange }),
+            React.createElement("button", { onClick: this.addItem }, "Add Item")));
     };
     return ToDoList;
 }(React.Component));
-window.onload = function () {
-    var el = document.getElementById('content');
-    var jsx = React.createElement("div", null, "Not hello anymore");
-    ReactDOM.render(jsx, el);
-};
 //# sourceMappingURL=app.js.map
--- TypeScriptTraining/TypeScriptTraining/app.tsx
+++ TypeScriptTraining/TypeScriptTraining/app.tsx
@@ -1,13 +1,92 @@
-﻿class ToDoList extends React.Component<void, void> {
+﻿window.onload = () => {
+    const el = document.getElementById('content');
+    const jsx = <ToDoList name="Brennan"/>;
+    ReactDOM.render(jsx, el);
+};
+
+interface Props {
+    name: string;
+}
+
+interface State {
+    items: Item[];
+    newItemName: string;
+}
+
+interface Item {
+    name: string;
+    completed: boolean;
+}
+
+class ToDoList extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            items: [
+                { name: "Milk", completed: false },
+                { name: "Eggs", completed: false },
+                { name: "Bread", completed: false },
+            ],
+            newItemName: ""
+        };
+    }
+
+    onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newState = {
+            ...this.state,
+            newItemName: e.currentTarget.value
+        };
+        this.setState(newState);
+    }
+
+    addItem = () => {
+        const newItems = [...this.state.items, { name: this.state.newItemName, completed: false }];
+        const newState = {
+            ...this.state,
+            items: newItems,
+            newItemName: ""
+        };
+        this.setState(newState);
+    }
+
+    toggleItemCompleted = (index: number) => {
+        const newItems = this.state.items.slice();
+        newItems[index] = {
+            ...newItems[index],
+            completed: !newItems[index].completed
+        };
+        const newState = {
+            ...this.state,
+            items: newItems
+        }
+        this.setState(newState);
+    }
+
     render() {
+        const renderItem = (item: Item, index: number) => {
+            let itemClass: string = item.completed ? "completed-item" : "incompleted-item";
+            itemClass += " todo-item";
+            const checkMark: string = item.completed ? " ✓" : "";
+            return (
+                <li>
+                    <div className={itemClass} onClick={() => this.toggleItemCompleted(index)}>
+                        {item.name}{checkMark}
+                    </div>
+                </li>
+            );
+        }
+
+        const itemsJSX = this.state.items.map((item, i) => renderItem(item, i));
+
         return (
-            <div>It seems there is still much to do.</div>
+            <div>
+                <h3>{this.props.name}'s To-Do List</h3>
+                <ul>
+                    {itemsJSX}
+                </ul>
+                <input value={this.state.newItemName} onChange={this.onInputChange} />
+                <button onClick={this.addItem} >Add Item</button>
+            </div>
         );
     }
 }
-
-window.onload = () => {
-    const el = document.getElementById('content');
-    const jsx = <div>Not hello anymore</div>;
-    ReactDOM.render(jsx, el);
-};
