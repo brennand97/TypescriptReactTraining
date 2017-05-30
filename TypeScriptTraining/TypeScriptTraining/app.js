07b693d0c3d6709a60ed90cc2ae0a042c46af679
Brennan Douglas <brennan.douglas@oregonstate.edu>
2017-05-30 13:58:07 -0700

    Stuck on bug

--- TypeScriptTraining/TypeScriptTraining/app.js
+++ TypeScriptTraining/TypeScriptTraining/app.js
@@ -1,6 +1,21 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var ToDoList = (function (_super) {
+    __extends(ToDoList, _super);
+    function ToDoList() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ToDoList.prototype.render = function () {
+        return (React.createElement("div", null, "It seems there is still much to do."));
+    };
+    return ToDoList;
+}(React.Component));
 window.onload = function () {
     var el = document.getElementById('content');
-    var jsx = React.createElement("div", null, "Hello, world!");
+    var jsx = React.createElement("div", null, "Not hello anymore");
     ReactDOM.render(jsx, el);
 };
 //# sourceMappingURL=app.js.map
--- TypeScriptTraining/TypeScriptTraining/app.tsx
+++ TypeScriptTraining/TypeScriptTraining/app.tsx
@@ -1,5 +1,13 @@
-﻿window.onload = () => {
+﻿class ToDoList extends React.Component<void, void> {
+    render() {
+        return (
+            <div>It seems there is still much to do.</div>
+        );
+    }
+}
+
+window.onload = () => {
     const el = document.getElementById('content');
-    const jsx = <div>Hello, world!</div>;
+    const jsx = <div>Not hello anymore</div>;
     ReactDOM.render(jsx, el);
 };
